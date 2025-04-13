94ec3983c03a15a95483e6625f6b0cb122a70de7
Alexander Pleshakov <sanya.pleshakov.95@mail.ru>
2025-04-14 00:02:16 +0300

    Initial Commit

---
+++ LinearProgrammingSimplex/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LinearProgrammingSimplex
+//
+//  Created by Александр Плешаков on 14.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LinearProgrammingSimplex/LinearProgrammingSimplexApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinearProgrammingSimplexApp.swift
+//  LinearProgrammingSimplex
+//
+//  Created by Александр Плешаков on 14.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct LinearProgrammingSimplexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
